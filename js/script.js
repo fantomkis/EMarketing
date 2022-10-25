86984228d6d4bcecd8cc60af41cc82281dda89a7
Pavlo Kysliak <pavel.kis0505@gmail.com>
2022-10-25 17:11:12 +0300

    add

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
 $(document).ready(function () {
-  $(".slick").slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });
+  $(".slick").slick({});
 });
